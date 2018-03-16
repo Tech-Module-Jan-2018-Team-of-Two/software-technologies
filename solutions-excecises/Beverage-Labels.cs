9c4e00e07bc349f8114d310cc5feedfebe2534da
g.kotsev00 <Kotsev75@gmail.com>
2018-03-16 23:39:10 +0200

    Adding solutions to the repository

---
+++ solutions-excecises/Beverage-Labels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyContent = int.Parse(Console.ReadLine());
+            int sugarContent = int.Parse(Console.ReadLine());
+
+            double calories = (volume / 100.0) * energyContent;
+            double weight = (volume / 100.0) * sugarContent;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{calories}kcal, {weight}g sugars");
+        }
+    }
+}
