b68df5e7b9ad476b7dacd03f63c6e0fb3f526a6b
g.kotsev00 <Kotsev75@gmail.com>
2018-03-18 10:39:56 +0200

    Commented on the solution of MilesToKilometres task

--- solutions-excecises/MilesToKilometres.cs
+++ solutions-excecises/MilesToKilometres.cs
@@ -6,8 +6,11 @@ namespace MilesToKilometres
     {
         static void Main(string[] args)
         {
+            //We receive miles from the Console
             double miles = Double.Parse(Console.ReadLine());
+            //We convert them to kilometers
             double convertMilesToKilometres = miles * 1.60934;
+            //Print result
             Console.WriteLine($"{convertMilesToKilometres:f2}");
         }
     }
