a1c3060bcf4431a13e0d1c6dbb2f0f9216108235
daughtryz <dimitar.iliev@abv.bg>
2018-03-18 10:18:28 +0200

    second attempt

---
+++ solutions-excecises/DebitCard.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CSharpProblemsLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            Console.Write($"{num1:d4} {num2:d4} {num3:d4} {num4:d4}");
+
+        }
+    }
+}
