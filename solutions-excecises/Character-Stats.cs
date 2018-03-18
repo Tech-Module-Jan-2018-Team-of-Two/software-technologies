e5f520ce3f1ab50d76687c0214503d698d21aa23
daughtryz <dimitar.iliev@abv.bg>
2018-03-18 10:39:22 +0200

    added a describtion

--- solutions-excecises/Character-Stats.cs
+++ solutions-excecises/Character-Stats.cs
@@ -6,12 +6,17 @@ namespace Character_Stats
     {
         static void Main(string[] args)
         {
+			// We receive a name from the console
             string name = Console.ReadLine();
+			
+			// We read the character's health and energy 
             int currentHealth = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
-
+			
+			
+			// Printing the result
             Console.WriteLine($"Name: {name}");
             Console.WriteLine($"Health: |{new string('|', currentHealth)}{new string('.', maxHealth - currentHealth)}|");
             Console.WriteLine($"Energy: |{new string('|', currentEnergy)}{new string('.', maxEnergy - currentEnergy)}|");
