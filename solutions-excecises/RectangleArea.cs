60df5e0e52255d93c6a980f9121d7976477cfd3a
g.kotsev00 <Kotsev75@gmail.com>
2018-03-18 10:38:25 +0200

    Comments added on rectangle area solution

--- solutions-excecises/RectangleArea.cs
+++ solutions-excecises/RectangleArea.cs
@@ -6,9 +6,12 @@ namespace RectangleArea
     {
         static void Main(string[] args)
         {
+            //We receive the width and the height of a rectangle
             float width = float.Parse(Console.ReadLine());
             float height = float.Parse(Console.ReadLine());
+            //Calculate the area
             float result = width * height;
+            //Print the result
             Console.WriteLine($"{result:f2}");
         }
     }
